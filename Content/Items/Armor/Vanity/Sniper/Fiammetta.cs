2bf7a8a0c45860df750dcc97c68adefca88e4789
2249438322 <2249438322@qq.com>
2025-09-29 22:55:41 +0800

    Fiammetta's Costume

---
+++ Content/Items/Armor/Vanity/Sniper/Fiammetta.cs
@@ -0,0 +1,15 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ArknightsMod.Content.Items.Armor.Vanity.Sniper
+{
+	[AutoloadEquip(EquipType.Head)]
+	public class FiammettaHead : ArknightsVanityHead { }
+
+	[AutoloadEquip(EquipType.Body)]
+	public class FiammettaBody : ArknightsVanityBody { }
+
+	[AutoloadEquip(EquipType.Legs)]
+	public class FiammettaLegs : ArknightsVanityLegs { }
+}
