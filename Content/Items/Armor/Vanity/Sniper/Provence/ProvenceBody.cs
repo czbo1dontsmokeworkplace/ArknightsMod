52c5d75464869f42e08e1b714ea4107661446244
nonrelian <3176803352@qq.com>
2025-12-23 17:16:03 +0800

    新增普罗旺斯时装

---
+++ Content/Items/Armor/Vanity/Sniper/Provence/ProvenceBody.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace ArknightsMod.Content.Items.Armor.Vanity.Sniper.Provence
+{
+	[AutoloadEquip(EquipType.Body)]
+	internal class ProvenceBody:ArknightsVanityBody
+	{
+		public override int Rarity => 5;
+		internal class ProvenceBodyLayer : PlayerDrawLayer
+		{
+			public override Position GetDefaultPosition() => new BeforeParent(PlayerDrawLayers.BackAcc);
+			public override bool GetDefaultVisibility(PlayerDrawSet drawInfo) {
+				Item body = new(ModContent.ItemType<ProvenceBody>());
+				return drawInfo.drawPlayer.body == body.bodySlot && !drawInfo.drawPlayer.dead;
+			}
+
+			protected override void Draw(ref PlayerDrawSet drawInfo) {
+
+				Texture2D texture = ModContent.Request<Texture2D>
+					("ArknightsMod/Content/Items/Armor/Vanity/Sniper/Provence/ProvenceBody_Tail").Value;
+
+				var offset = new Vector2(-16, 12);
+
+				int drawX = (int)(drawInfo.drawPlayer.MountedCenter.X + offset.X * drawInfo.drawPlayer.direction - Main.screenPosition.X);
+				int drawY = (int)(drawInfo.drawPlayer.MountedCenter.Y + offset.Y - Main.screenPosition.Y);
+				int dyeShader = drawInfo.drawPlayer.dye?[0].dye ?? 0;
+				float offsetY = 0;
+				if (drawInfo.drawPlayer.bodyFrame.Y >= 7 * drawInfo.drawPlayer.bodyFrame.Height &&
+					drawInfo.drawPlayer.bodyFrame.Y <= 9 * drawInfo.drawPlayer.bodyFrame.Height ||
+					drawInfo.drawPlayer.bodyFrame.Y >= 14 * drawInfo.drawPlayer.bodyFrame.Height &&
+					drawInfo.drawPlayer.bodyFrame.Y <= 16 * drawInfo.drawPlayer.bodyFrame.Height) {
+					offsetY = -2;
+				}
+				drawInfo.DrawDataCache.Add(
+					new DrawData(texture, new Vector2(drawX, drawY + offsetY + drawInfo.drawPlayer.gfxOffY),
+					null, drawInfo.colorArmorBody, 0f, texture.Size() * 0.5f, 1f, drawInfo.drawPlayer.direction == 1 ? SpriteEffects.None : SpriteEffects.FlipHorizontally, 0) {
+						shader = dyeShader
+					});
+			}
+		}
+	}
+}
---
+++ Content/Items/Armor/Vanity/Sniper/Provence/ProvenceHead.cs
@@ -0,0 +1,24 @@
+﻿using ArknightsMod.Content.Items.Armor.Vanity;
+using ArknightsMod.Content.Items.Armor.Vanity.Sniper.Melanite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ArknightsMod.Content.Items.Armor.Vanity.Sniper.Provence
+{
+	[AutoloadEquip(EquipType.Head)]
+	internal class ProvenceHead:ArknightsVanityHead
+	{
+		public override int Rarity => 5;
+		public override bool IsArmorSet(Item head, Item body, Item legs) {
+			return body.type == ModContent.ItemType<ProvenceBody>() && legs.type == ModContent.ItemType<ProvenceLegs>();
+		}
+	}
+}
