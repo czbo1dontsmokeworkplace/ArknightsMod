354f7ff8ffda98286ee12e64edb4480db414c38e
2249438322 <2249438322@qq.com>
2026-02-07 21:54:39 +0800

    新增玛恩纳时装

---
+++ Content/Items/Armor/Vanity/Guard/Mlynar/MlynarBody.cs
@@ -0,0 +1,15 @@
+﻿using Terraria.ModLoader;
+using Terraria;
+
+namespace ArknightsMod.Content.Items.Armor.Vanity.Guard.Mlynar
+{
+	[AutoloadEquip(EquipType.Body)]
+	public class MlynarBody : ArknightsVanityBody
+	{
+		public override int Rarity => 6;
+		public override int Value => 560000;
+		public override void UpdateEquip(Player player) {
+
+		}
+	}
+}
---
+++ Content/Items/Armor/Vanity/Guard/Mlynar/MlynarHead.cs
@@ -0,0 +1,18 @@
+﻿using Terraria.ModLoader;
+using Terraria;
+
+namespace ArknightsMod.Content.Items.Armor.Vanity.Guard.Mlynar
+{
+	[AutoloadEquip(EquipType.Head)]
+	public class MlynarHead : ArknightsVanityHead
+	{
+		public override int Rarity => 6;
+		public override int Value => 560000;
+		public override void UpdateEquip(Player player) {
+
+		}
+		public override bool IsArmorSet(Item head, Item body, Item legs) {
+			return body.type == ModContent.ItemType<MlynarBody>() && legs.type == ModContent.ItemType<MlynarLegs>();
+		}
+	}
+}
