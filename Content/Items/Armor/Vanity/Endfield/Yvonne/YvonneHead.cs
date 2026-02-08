6b112968dfcfff2daaa1bfe1e51631e70f8cb9af
2249438322 <2249438322@qq.com>
2026-02-08 12:20:24 +0800

    伊冯时装，明日方舟终末地也是明日方舟

---
+++ Content/Items/Armor/Vanity/Endfield/Yvonne/YvonneHead.cs
@@ -0,0 +1,19 @@
+﻿using Terraria.ModLoader;
+using Terraria;
+
+namespace ArknightsMod.Content.Items.Armor.Vanity.Endfield.Yvonne
+{
+	[AutoloadEquip(EquipType.Head)]
+	public class YvonneHead : ArknightsVanityHead
+	{
+		public override int Rarity => 6;
+		public override int Value => 560000;
+		public override void UpdateEquip(Player player) {
+
+		}
+		public override bool IsArmorSet(Item head, Item body, Item legs) {
+			return body.type == ModContent.ItemType<YvonneBody>() &&
+				legs.type == ModContent.ItemType<YvonneLegs>();
+		}
+	}
+}
