50f5d7c771eb60ea419d5a4045789d08fee873e6
fuhua45 <3624952391@qq.com>
2026-02-23 13:28:57 +0800

    修改了投币玩具，骑士戒律，金酒之杯的攻速无法生效问题。增加了两种生息演算的食物以及合成配方和本地化填充。修改了王命凡形不生效的问题。增加了庞贝召唤物的合成配方以及召唤方式。修改了蟹肉拌饭效果异常，贴图错误。将Content.Item.foods文件里的文件移动位置并删除foods文件夹
    t/COMMIT_EDITMSG [unix] (16:59 23/02/2026)
    85,1 Bot/COMMIT_EDITMSG [unix] (16:59 23/02/2026)
    85,1 Bo#

---
+++ Content/Items/BossSummon/PompeiiSummon.cs
@@ -0,0 +1,49 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using ArknightsMod.Content.NPCs.Enemy.OF.Pmp;
+using Terraria.Audio;
+
+namespace ArknightsMod.Content.Items.BossSummon
+{
+	public class PompeiiSummon : ModItem
+	{
+		public override void AddRecipes() {
+			
+			CreateRecipe()
+				.AddIngredient<Material.OrirockCube>(1)
+				.AddIngredient(22, 5)
+                .AddIngredient(173,1)
+				.Register();
+		}
+
+	
+	public override void SetDefaults() {
+			Item.width = 40;
+			Item.height = 40;
+			Item.maxStack = 1;
+			Item.value = 325;
+			Item.rare = 1;
+			Item.useAnimation = 30;
+			Item.useTime = 30;
+			Item.useStyle = ItemUseStyleID.HoldUp;
+			Item.consumable = false;
+		}
+		public override bool CanUseItem(Player player) {
+			return
+			!NPC.AnyNPCs(ModContent.NPCType<Pompeii>());
+		}
+		private int type;
+		public override bool? UseItem(Player player) {
+			if (player.whoAmI == Main.myPlayer) {
+				SoundEngine.PlaySound(SoundID.Roar, player.position);
+				type = ModContent.NPCType<Pompeii>();
+			}
+			if (Main.netMode != NetmodeID.MultiplayerClient) {
+				NPC.SpawnOnPlayer(player.whoAmI, type);
+			}
+			
+			return true;
+		}
+	}
+}
