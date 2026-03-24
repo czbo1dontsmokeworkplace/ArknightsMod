7185fb63e0caa26df44b877a1569af3c5a8d6c3c
MorniaOwO <c1594962681@gmail.com>
2026-03-24 12:48:13 +0800

    Update ExsusiaAlterDefault.cs

--- Content/Items/Armor/Vanity/Specialist/ExusiaiAlter/ExsusiaAlterDefault.cs
+++ Content/Items/Armor/Vanity/Specialist/ExusiaiAlter/ExsusiaAlterDefault.cs
@@ -6,7 +6,7 @@ namespace ArknightsMod.Content.Items.Consumables.VanityBags
 {
 	public class ExusiaiAlterDefault : ArknightsVanityBag
 	{
-		public override string Texture => "ArknightsMod/Content/Items/Consumables/VanityBags/ExusiaiDefault";
+		public override string Texture => "ArknightsMod/Content/Items/Armor/Vanity/Specialist/ExusiaiAlter/ExsusiaAlterDefault";
 
 		protected override List<int> GetItems() {
 			return new List<int>
