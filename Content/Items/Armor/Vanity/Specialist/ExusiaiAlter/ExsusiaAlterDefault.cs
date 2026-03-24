ada0f266a723d2eee786dddf3d55157c100ea563
MorniaOwO <c1594962681@gmail.com>
2026-03-24 09:02:22 +0800

    补齐新约能天使时装的中文化和三合一时装包，并添加进档案袋抽奖系统

---
+++ Content/Items/Armor/Vanity/Specialist/ExusiaiAlter/ExsusiaAlterDefault.cs
@@ -0,0 +1,20 @@
+using ArknightsMod.Content.Items.Armor.Vanity.Specialist.ExusiaiAlter;
+using System.Collections.Generic;
+using Terraria.ModLoader;
+
+namespace ArknightsMod.Content.Items.Consumables.VanityBags
+{
+	public class ExusiaiAlterDefault : ArknightsVanityBag
+	{
+		public override string Texture => "ArknightsMod/Content/Items/Consumables/VanityBags/ExusiaiDefault";
+
+		protected override List<int> GetItems() {
+			return new List<int>
+			{
+			ModContent.ItemType<ExusiaiAlterHead>(),
+			ModContent.ItemType<ExusiaiAlterBody>(),
+			ModContent.ItemType<ExusiaiAlterLegs>()
+		};
+		}
+	}
+}
