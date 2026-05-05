1935307e331c581a4c61f544694f35e226c54c82
nonrelian <3176803352@qq.com>
2026-05-05 21:18:00 +0800

    新增宴的装甲，后续需修改合成表和本地化

---
+++ Content/Items/Armor/Vanity/Guard/Utage/Armor/ArmorUtageBody.cs
@@ -0,0 +1,24 @@
+﻿
+using ArknightsMod.Content.Items.Armor.Vanity.Guard.Melantha;
+using ArknightsMod.Content.Tiles.Infrastructure;
+using Terraria.ModLoader;
+
+namespace ArknightsMod.Content.Items.Armor.Vanity.Guard.Utage.Armor
+{
+	[AutoloadEquip(EquipType.Body)]
+	internal class ArmorUtageBody : ArknightsArmorBody
+	{
+		public override int Rarity => 4;
+		public override int LifeBonus => 98;
+		public override void SetArmorDefaults() {
+			Item.defense = 16;
+		}
+		public override void AddRecipes() {
+			CreateRecipe()
+			.AddIngredient(ModContent.ItemType<UtageBody>(), 1)
+			.AddIngredient(ModContent.ItemType<Orundum>(), 30)
+			.AddTile(ModContent.TileType<FactoryTile>())
+			.Register();
+		}
+	}
+}
