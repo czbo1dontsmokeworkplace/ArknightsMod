a97e52e1f78d0e514d2fbaf6c2104e37a0da2106
MorniaOwO <c1594962681@gmail.com>
2026-05-07 21:39:19 +0800

    能天使时装更新（代栎安推送）

---
+++ Content/Items/Armor/Vanity/Sniper/Exusiai/Armor/ArmorExusiaiBody.cs
@@ -0,0 +1,24 @@
+﻿
+using ArknightsMod.Content.Items.Armor.Vanity.Guard.Utage;
+using ArknightsMod.Content.Tiles.Infrastructure;
+using Terraria.ModLoader;
+
+namespace ArknightsMod.Content.Items.Armor.Vanity.Sniper.Exusiai.Armor
+{
+	[AutoloadEquip(EquipType.Body)]
+	internal class ArmorExusiaiBody:ArknightsArmorBody
+	{
+		public override int Rarity => 6;
+		public override int LifeBonus => 84;
+		public override void SetArmorDefaults() {
+			Item.defense = 12;
+		}
+		public override void AddRecipes() {
+			CreateRecipe()
+			.AddIngredient(ModContent.ItemType<UtageBody>(), 1)
+			.AddIngredient(ModContent.ItemType<Orundum>(), 30)
+			.AddTile(ModContent.TileType<FactoryTile>())
+			.Register();
+		}
+	}
+}
