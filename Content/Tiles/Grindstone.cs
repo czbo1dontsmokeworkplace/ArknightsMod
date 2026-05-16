152f77d9254edde9dcf79b51103ff610c6b6abe4
2249438322 <2249438322@qq.com>
2026-05-16 13:11:27 +0800

    Merge 05162026

--- Content/Tiles/Grindstone.cs
+++ Content/Tiles/Grindstone.cs
@@ -43,29 +43,10 @@ namespace ArknightsMod.Content.Tiles
 		public override void SetStaticDefaults() {
 			GrindstonePassMessage = Language.GetOrRegister(Mod.GetLocalizationKey($"WorldGen.{nameof(GrindstonePassMessage)}"));
 		}
-
-		// World generation is explained more in https://github.com/tModLoader/tModLoader/wiki/World-Generation
-		public override void ModifyWorldGenTasks(List<GenPass> tasks) {
-			// Because world generation is like layering several images on top of each other, we need to do some steps between the original world generation steps.
-
-			// Most vanilla ores are generated in a step called "Shinies", so for maximum compatibility, we will also do this.
-			// First, we find out which step "Shinies" is.
-=======
-			GrindstonePassMessage = Language.GetOrRegister(Mod.GetLocalizationKey($"WorldGen.{nameof(GrindstonePassMessage)}"));
-		}
-
-		// World generation is explained more in https://github.com/tModLoader/tModLoader/wiki/World-Generation
-		public override void ModifyWorldGenTasks(List<GenPass> tasks) {
-			// Because world generation is like layering several images on top of each other, we need to do some steps between the original world generation steps.
-
-			// Most vanilla ores are generated in a step called "Shinies", so for maximum compatibility, we will also do this.
-			// First, we find out which step "Shinies" is.
->>>>>>> Stashed changes:Content/Tiles/Grindstone.cs
+		public override void ModifyWorldGenTasks(List<GenPass> tasks)
+		{
 			int ShiniesIndex = tasks.FindIndex(genpass => genpass.Name.Equals("Shinies"));
-
 			if (ShiniesIndex != -1) {
-				// Next, we insert our pass directly after the original "Shinies" pass.
-				// ExampleOrePass is a class seen bellow
 				tasks.Insert(ShiniesIndex + 1, new GrindPass("Grindstone", 237.4298f));
 			}
 		}
--- Content/Tiles/RMA7012.cs
+++ Content/Tiles/RMA7012.cs
@@ -46,22 +46,9 @@ namespace ArknightsMod.Content.Tiles
 		}
 
 		public override void ModifyWorldGenTasks(List<GenPass> tasks) {
-			// Because world generation is like layering several images ontop of each other, we need to do some steps between the original world generation steps.
-
-			// The first step is an Ore. Most vanilla ores are generated in a step called "Shinies", so for maximum compatibility, we will also do this.
-			// First, we find out which step "Shinies" is.
-=======
-		public override void ModifyWorldGenTasks(List<GenPass> tasks) {
-			// Because world generation is like layering several images ontop of each other, we need to do some steps between the original world generation steps.
-
-			// The first step is an Ore. Most vanilla ores are generated in a step called "Shinies", so for maximum compatibility, we will also do this.
-			// First, we find out which step "Shinies" is.
->>>>>>> Stashed changes:Content/Tiles/RMA7012.cs
 			int ShiniesIndex = tasks.FindIndex(genpass => genpass.Name.Equals("Shinies"));
 
 			if (ShiniesIndex != -1) {
-				// Next, we insert our pass directly after the original "Shinies" pass.
-				// ExampleOrePass is a class seen bellow
 				tasks.Insert(ShiniesIndex + 1, new RMA12Pass("RMA70-12", 237.4298f));
 			}
 		}
