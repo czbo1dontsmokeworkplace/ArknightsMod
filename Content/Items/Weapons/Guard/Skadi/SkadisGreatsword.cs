b0f55bb427f95940ee26b583ad58859280f07780
MorniaOwO <c1594962681@gmail.com>
2026-06-13 13:04:16 +0800

    更新澄闪、史都华德、杜宾、霜叶、红豆、玫兰莎、武器贴图；修复斯卡蒂的巨剑技能无法开启问题并更新技能图标。

--- Content/Items/Weapons/Guard/Skadi/SkadisGreatsword.cs
+++ Content/Items/Weapons/Guard/Skadi/SkadisGreatsword.cs
@@ -1,7 +1,6 @@
-using ArknightsMod.Content.Items.Weapons;
 using ArknightsMod.Players;
-using ArknightsMod.Systems.Gameplay.Skill;
 using Terraria;
+using Terraria.Audio;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -10,6 +9,15 @@ namespace ArknightsMod.Content.Items.Weapons.Guard.Skadi
 	public class SkadisGreatsword : ExpansionWeaponBase
 	{
 		protected override int[] EliteDamage => [78, 96, 113];
+
+		private static SoundStyle SkillActiveSfx;
+
+		public override void Load() {
+			SkillActiveSfx = new SoundStyle("ArknightsMod/Sounds/SkillActive1") {
+				Volume = 0.5f,
+				MaxInstances = 2,
+			};
+		}
 
 		public override void SetDefaults() {
 			Item.damage = EliteDamage[0];
@@ -21,19 +29,35 @@ namespace ArknightsMod.Content.Items.Weapons.Guard.Skadi
 			Item.knockBack = 8f;
 			Item.value = Item.sellPrice(gold: 1);
 			Item.rare = ItemRarityID.Green;
-			Item.autoReuse = true;
+			Item.autoReuse = false;
 			Item.useStyle = ItemUseStyleID.Swing;
 			Item.crit = 4;
 		}
 
+		public override bool AltFunctionUse(Player player) => true;
+
 		public override bool CanUseItem(Player player) {
 			var mp = player.GetModPlayer<WeaponPlayer>();
+
+			// 右键：激活技能
+			if (player.altFunctionUse == 2) {
+				if (mp.StockCount > 0 && !mp.SkillActive) {
+					mp.SkillActive = true;
+					mp.SkillTimer = 0;
+					mp.DelStockCount();
+					SoundEngine.PlaySound(SkillActiveSfx, player.Center);
+				}
+				return false;
+			}
+
+			// 左键攻击伤害
 			if (mp.SkillActive && mp.Skill == 1) {
-				// S2：攻击力+170%，简化为伤害倍率提升
+				// S2 涌潮悲歌：攻击力+170%
 				Item.damage = (int)(EliteDamage[EliteStage] * 2.7f);
 			} else {
 				Item.damage = EliteDamage[EliteStage];
 			}
+
 			return base.CanUseItem(player);
 		}
 	}
