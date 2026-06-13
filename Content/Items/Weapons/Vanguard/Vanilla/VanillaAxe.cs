f0b8b720e7d697c5465f573f2ad324c5cf76fa7a
MorniaOwO <c1594962681@gmail.com>
2026-06-13 18:13:00 +0800

    优化适配香草的巨斧。

--- Content/Items/Weapons/Vanguard/Vanilla/VanillaAxe.cs
+++ Content/Items/Weapons/Vanguard/Vanilla/VanillaAxe.cs
@@ -1,6 +1,7 @@
 using ArknightsMod.Content.Items.Weapons;
 using ArknightsMod.Players;
 using Terraria;
+using Terraria.Audio;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -9,6 +10,12 @@ namespace ArknightsMod.Content.Items.Weapons.Vanguard.Vanilla
 	public class VanillaAxe : ExpansionWeaponBase
 	{
 		protected override int[] EliteDamage => [31, 43];
+
+		private static SoundStyle SkillActiveSfx;
+
+		public override void Load() {
+			SkillActiveSfx = new SoundStyle("ArknightsMod/Sounds/SkillActive1") { Volume = 0.5f, MaxInstances = 2 };
+		}
 
 		public override void SetDefaults() {
 			Item.damage = EliteDamage[0];
@@ -25,10 +32,27 @@ namespace ArknightsMod.Content.Items.Weapons.Vanguard.Vanilla
 			Item.crit = 4;
 		}
 
+		public override bool AltFunctionUse(Player player) => true;
+
 		public override bool CanUseItem(Player player) {
 			var mp = player.GetModPlayer<WeaponPlayer>();
-			Item.damage = mp.SkillActive ? (int)(EliteDamage[EliteStage] * 1.35f) : EliteDamage[EliteStage];
+			if (player.altFunctionUse == 2) {
+				if (mp.StockCount > 0 && !mp.SkillActive) {
+					mp.SkillActive = true;
+					mp.SkillTimer = 0;
+					mp.DelStockCount();
+					SoundEngine.PlaySound(SkillActiveSfx, player.Center);
+				}
+				return false;
+			}
 			return base.CanUseItem(player);
+		}
+
+		public override void ModifyWeaponDamage(Player player, ref StatModifier damage) {
+			base.ModifyWeaponDamage(player, ref damage);
+			var mp = player.GetModPlayer<WeaponPlayer>();
+			if (mp.SkillActive)
+				damage *= 1.35f;
 		}
 	}
 }
